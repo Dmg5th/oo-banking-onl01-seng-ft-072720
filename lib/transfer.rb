212624518b20b0d4176047f29a69116334f5087f
Daniel Gould <dmgould310@gmail.com>
2020-08-11 21:17:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,16 +12,17 @@ class Transfer
       self.sender.valid? && self.receiver.valid? 
     end 
     
-    def execute_transaction 
-      if valid? && self.status == 'pending' && sender.balance > amount
-        sender.balance -= amount
-        receiver.balance += amount
-        self.status = 'complete'
-      else 
-        self.status = "rejected" 
-        "Transaction rejected. Please check your account balance."
-      end 
-    end 
+      def execute_transaction
+    # binding.pry
+    if valid? && self.status == 'pending' && sender.balance > amount
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = 'complete'
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+    
     
 
     
