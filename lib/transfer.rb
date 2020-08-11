e0dd5254af2d0b5247bd2e1e75cd8740581e219b
Daniel Gould <dmgould310@gmail.com>
2020-08-11 21:19:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,20 +1,18 @@
-require 'pry'
-class Transfer
-  attr_reader :sender, :receiver, :amount
-  attr_accessor :status
-  
-  def initialize(sender, receiver, amount, status = 'pending')
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
-    @status = status
-  end 
-
-  def valid?
-    self.sender.valid? && self.receiver.valid?
-  end 
-
-  def execute_transaction
+class Transfer 
+    attr_accessor :sender, :receiver, :status, :amount  
+    
+    def initialize(sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @amount = 50 
+      @status = "pending"
+    end 
+    
+    def valid?
+      self.sender.valid? && self.receiver.valid? 
+    end 
+    
+      def execute_transaction
     # binding.pry
     if valid? && self.status == 'pending' && sender.balance > amount
       sender.balance -= amount
@@ -24,15 +22,18 @@ class Transfer
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
-
-  end
-
-  def reverse_transfer 
-    while valid? && self.status == 'complete' && receiver.balance > amount
-      sender.balance += amount 
-      receiver.balance -= amount
-      self.status = 'reversed'
-    end
   end 
     
-end
+    
+
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end 
