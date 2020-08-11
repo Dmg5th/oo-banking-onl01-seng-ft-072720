fcda41f8d3a44443d99f98a0f74feb4827b54d64
Daniel Gould <dmgould310@gmail.com>
2020-08-11 21:00:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,27 @@
-class Transfer
-  # your code here
-end
+class Transfer 
+    attr_accessor :sender, :receiver, :status, :amount  
+    
+    def initialize(sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @amount = 50 
+      @status = "pending"
+    end 
+    
+    def valid?
+      self.sender.valid? && self.receiver.valid? 
+    end 
+    
+    def execute_transaction 
+      self.sender -= amount 
+      self.receiver += amount 
+    end 
+  
+  
+  
+  
+  
+  
+  
+  
+end 
