c696ccf0b5522317ff3cfb86c2759e9f741f242f
Daniel Gould <dmgould310@gmail.com>
2020-08-11 21:29:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
       end 
       
       def reverse_transfer
-        while valid? && self.status 
+        while valid? && self.status == "complete" && sender.balance > amount 
       
 
     
